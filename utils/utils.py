d59898b18be5be1b51004c05e5226d8798a99dd1
Ihor Magur <ihor.magur@devabit.com>
2020-03-20 13:45:29 +0200

    Added words calculation

--- utils/utils.py
+++ utils/utils.py
@@ -1,19 +1,37 @@
-def get_users_wh(raw_data):
+import re
+
+USER = 13
+WORDCOUNT = 8
+
+
+def get_clean_data(raw_data):
     if not raw_data:
         return None
-    # 8 wordcount, 13 user
-    return [[row[13], row[8]] for row in raw_data[1:] if row[0]]
+    return [row for row in raw_data[1:] if row[0]]
 
 
 def collect_users_wh(raw_data):
-    clean_data = get_users_wh(raw_data)
+    clean_data = get_clean_data(raw_data)
     if not clean_data:
         return None
     data = dict()
     for r in clean_data:
-        try:
-            data[r[0]]["words"] += int(r[1])
-            data[r[0]]["tasks"] += 1
-        except KeyError:
-            data[r[0]] = {"words": int(r[1]), "tasks": 1}
+        names = re.split(',(?![0-9])(?! [0-9])', r[USER])
+        words = int(r[WORDCOUNT])
+        for name in names:
+            name = name.strip()
+            if (idx := name.find('(')) != -1:
+                r_name = name
+                name = r_name[:idx].strip()
+                split_words = r_name[idx+1:-1].strip()
+                split_words = split_words.replace(',', '').replace('.', '').replace(' ', '')
+                split_words = int(split_words)
+            else:
+                split_words = words // len(names)
+
+            try:
+                data[name]["words"] += split_words
+                data[name]["tasks"] += 1
+            except KeyError:
+                data[name] = {"words": split_words, "tasks": 1}
     return sorted([(k, v["words"], v["tasks"]) for k, v in data.items()], key=lambda i: i[0])
